45123c15c70eae082f8e7a197f146282536df573
AbhinavCode <dancewithnaniyt@gmail.com>
2025-11-02 10:15:09 +0530

    Update Voice_ChatBot final.py

--- Voice_ChatBot final.py
+++ Voice_ChatBot final.py
@@ -41,7 +41,7 @@ safety_settings = [
     {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
 ]
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-pro",
+    model_name="gemini-2.5-pro",
     generation_config=generation_config,
     safety_settings=safety_settings
 )
@@ -89,3 +89,4 @@ while True:
             print("Please say 'hello' to start the conversation.")
     else:
         print("No speech detected.")
+
